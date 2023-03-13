4a6b019cb300ef89015b6bc5bf2911123e07b23b
atgane <hyper2012@naver.com>
2023-03-13 21:58:31 +0900

    Feat: window + wsl auto install impl

--- exec_on_window.bat
+++ exec_on_window.bat
@@ -1,3 +1,12 @@
-wsl ./init_wsl.sh
+: wsl go install
+wsl /bin/bash -lic "./init_wsl.sh"
+
+: wsl kind install
+wsl /bin/bash -lic "./install_kind.sh"
+
+: restart docker desktop && wsl
 wsl --terminate Ubuntu-22.04
-wsl ./get_go_env.sh
+@echo off
+echo Restarting Docker Desktop...
+"C:\Program Files\Docker\Docker\Docker Desktop.exe" -t
+echo Docker Desktop has been restarted.
--- init_wsl.sh
+++ init_wsl.sh
@@ -1,4 +1,3 @@
-echo "hello world!"
 sudo apt update && sudo apt upgrade -y
 
 wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
@@ -7,5 +6,7 @@ rm go1.20.2.linux-amd64.tar.gz
 
 echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a ~/.bashrc
 source ~/.bashrc
+
+go env
 
 exit
